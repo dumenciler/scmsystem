ded24dd84eadda3722cd78f09113eb66e6ad40f3
Ozgur Agar <ozguragarr@gmail.com>
2025-10-29 19:21:37 +0300

    Initial commit

---
+++ src/test/java/com/dumenciler/scmsystem/ScmsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumenciler.scmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
